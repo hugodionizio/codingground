78c238171ed1cdad0d82b0b43b69495287065d68
Hugo Dionizio Santos <hugo@hugo-Inspiron-3442>
2015-05-06 16:00:43 -0300

    Hello GitHub!!!

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Hugo Dionizio Santos
+// Version     :
+// Copyright   : All Right Left
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello C++!!!" << endl; // prints !!!Hello C++!!!
+	return 0;
+}
