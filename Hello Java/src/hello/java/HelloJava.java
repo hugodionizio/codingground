7903d91c7fa3d5d076df0eb85a51010d0dd3d504
Hugo Dionizio Santos <hugo@hugo-Inspiron-3442>
2015-05-06 17:46:46 -0300

    Hello Java - GitHub!

---
+++ Hello Java/src/hello/java/HelloJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.java;
+/**
+ *
+ * @author Hugo Dionizio Santos
+ */
+
+import javax.swing.JOptionPane;
+
+public class HelloJava {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Oi Java!", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
+    }    
+}
